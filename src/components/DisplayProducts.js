33dc1b8ef77c6e16756185112f1603d74c799a16
LucasCambon <lucambon08@gmail.com>
2023-01-31 02:30:43 -0300

    cart products preview

--- src/components/DisplayProducts.js
+++ src/components/DisplayProducts.js
@@ -1,70 +1,28 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import ButtonBase from '@mui/material/ButtonBase';
+import ProductCart from './ProductCart';
 
-const Img = styled('img')({
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-});
   
 
 export default function DisplayProducts(){
 
+    const products = [1,2,3]
 
     return(
         <Box sx={{ flexGrow: 1, marginTop: "50px" }}>
-            <Typography color="#1a2634" variant="h4" gutterBottom>
+            <Typography sx={{marginLeft: "25px"}} color="#1a2634" variant="h4" gutterBottom>
                 Your cart
             </Typography>
-            <Paper
-            sx={{
-                p: 2,
-                margin: 'auto',
-                maxWidth: 500,
-                flexGrow: 1,
-                backgroundColor: (theme) =>
-                theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-            }}
-            >
-                <Grid container spacing={2}>
-                    <Grid item>
-                    <ButtonBase sx={{ width: 128, height: 128 }}>
-                        <Img alt="complex" src="/static/images/grid/complex.jpg" />
-                    </ButtonBase>
-                    </Grid>
-                    <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                        <Grid item xs>
-                        <Typography gutterBottom variant="subtitle1" component="div">
-                            Standard license
-                        </Typography>
-                        <Typography variant="body2" gutterBottom>
-                            Full resolution 1920x1080 • JPEG
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            ID: 1030114
-                        </Typography>
-                        </Grid>
-                        <Grid item>
-                        <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                            Remove
-                        </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="subtitle1" component="div">
-                        $19.00
-                        </Typography>
-                    </Grid>
-                    </Grid>
-                </Grid>
-            </Paper>
+            
+            {
+                products.map((row,i) => {
+                    return (
+                        <ProductCart key={row+i} />
+                    )
+                })
+            }
+            
         </Box>
     )
 }
--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -10,51 +10,48 @@ export default function OrderSummary(){
 
     return(
         <Box sx={{ flexGrow: 1, marginTop: "50px" }}>
-            <Typography color="#1a2634" variant="h4" gutterBottom>
+            <Typography sx={{marginLeft: "25px"}} color="#1a2634" variant="h4" gutterBottom>
                 Order Summary
             </Typography>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid xs={6} md={6}>
-                        <Typography color='#979da3' variant="subtitle1" gutterBottom>
-                            Number of items
-                        </Typography>
+            <Grid container>
+                <Grid sx={{margin: 'auto'}} item xs={10}>
+                    <Grid container xs={10}>
+                        <Grid xs={8}>
+                            <Typography color='#979da3' variant="subtitle1" gutterBottom>
+                                Number of items
+                            </Typography>
+                        </Grid>
+                        <Grid xs={4}>
+                            <Typography textAlign="right" color='#979da3' variant="subtitle1" gutterBottom>
+                                3
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Divider />
+                        </Grid>
                     </Grid>
-                    <Grid xs={2} md={2}>
-                        <Typography textAlign="right" color='#979da3' variant="subtitle1" gutterBottom>
-                            3
-                        </Typography>
+                    <Grid container spacing={2} xs={10}>
+                        <Grid xs={2}>
+                            <Typography fontWeight="bold" color="#1a2634" variant="h6" gutterBottom>
+                                Total:
+                            </Typography>
+                        </Grid>
+                        <Grid xs={10}>
+                            <Typography fontWeight="bold" textAlign="right" color="#1a2634" variant="h6" gutterBottom>
+                                $ 3,560.00
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid xs={8} md={8}>
-                        <Divider />
-                    </Grid>
-                </Grid>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid xs={2} md={2}>
-                        <Typography fontWeight="bold" color="#1a2634" variant="h6" gutterBottom>
-                            Total:
-                        </Typography>
-                    </Grid>
-                    <Grid xs={6} md={6}>
-                        <Typography fontWeight="bold" textAlign="right" color="#1a2634" variant="h6" gutterBottom>
-                            $ 3,560.00
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid xs={8} md={8}>
-                        <Button sx={{ width: "100%", height: "50px", backgroundColor: "#3a4451", fontWeight: "bold" }} variant="contained">Proceed to Checkout</Button>
-                    </Grid>
-                    <Grid xs={8} md={8}>
-                        <Button sx={{ width: "100%", height: "50px", color: "#3a4451", borderColor: "#3a4451", borderWidth: "3px", fontWeight: "bold" }} variant="outlined">Continue Shopping</Button>
+                    <Grid container spacing={2} xs={10}>
+                        <Grid xs={12}>
+                            <Button sx={{ width: "100%", height: "50px", backgroundColor: "#3a4451", fontWeight: "bold" }} variant="contained">Proceed to Checkout</Button>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Button sx={{ width: "100%", height: "50px", color: "#3a4451", borderColor: "#3a4451", borderWidth: "3px", fontWeight: "bold" }} variant="outlined">Continue Shopping</Button>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Box>
-            
+            </Grid>
         </Box>
     )
 }
